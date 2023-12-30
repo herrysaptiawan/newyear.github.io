88b99e4776d922dcd5ad699a7a3468cf0f72de1a
herrysaptiawan <87358997+herrysaptiawan@users.noreply.github.com>
2023-12-30 11:03:25 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,55 +1,52 @@
-// Countdown script
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
-const fireworkContainer = document.querySelector('.fireworks-container')
+const   second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24,
+        fireworkContainer = document.querySelector('.fireworks-container')
 
-let current = new Date().getTime();
-let countdown = new Date('2024-01-01 00:00:00').getTime();
-let interval = setInterval(function() {
-let distance = countdown - current;
+// SET DATE NEW YEAR
+let new_year = "Jan 1, 2024 00:00:00",
 
-if (distance <= 59000) {
-document.getElementById('full').style.display = 'none';
-document.getElementById('half').style.display = 'block';
+    countDown = new Date(new_year).getTime(),
+    interval = setInterval(function() {    
+  
+        let now = new Date().getTime(),
+        distance = countDown - now;
+        
+        document.getElementById("days").innerText = pad(Math.floor(distance / (day))),
+        document.getElementById("hours").innerText = pad(Math.floor((distance % (day)) / (hour))),
+        document.getElementById("minutes").innerText = pad(Math.floor((distance % (hour)) / (minute))),
+        document.getElementById("seconds").innerText = pad(Math.floor((distance % (minute)) / second)),
+        document.getElementById("new_seconds").innerText = new_pad(Math.floor((distance % (minute)) / second));
+        
+        if (distance <= 60000) {
+            document.getElementById("full").style.display = "none";
+            document.getElementById("half").style.display = "block";
 
-  if (distance <= 0) {
-    clearInterval(interval);
-    fireworks.start();
-    playAnimation();
-    document.getElementById('half').style.display = 'none';
-  }
-}
-  document.getElementById('last').getElementsByClassName('new_digit')[0].innerHTML = new_pad(parseInt((distance % minute) / second));
-  document.getElementById('day').getElementsByClassName('digit')[0].innerHTML = pad(parseInt(distance / day));
-  document.getElementById('hour').getElementsByClassName('digit')[0].innerHTML = pad(parseInt((distance % day)/ hour));
-  document.getElementById('min').getElementsByClassName('digit')[0].innerHTML = pad(parseInt((distance % hour) / minute));
-  document.getElementById('sec').getElementsByClassName('digit')[0].innerHTML = pad(parseInt((distance % minute) / second));
-  current += second;
-}, second);
+            if (distance <= 0) {
+            document.getElementById("new-year-text").style.display = "block";
+            document.getElementById("countdown").style.display = "none";
+            document.getElementById("year_text").style.display = "block";
+            document.getElementById("half").style.display = "none";      
+            fireworks.start();      
+            clearInterval(interval);
+            }
+        }
+    }, 0)
 
 function pad(n) {
-  return (n < 10 ? '0' : '') + n;
+return (n < 10 ? '0' : '') + n;
 }
-
+            
 function new_pad(x) {
-  return (x < 10 ? '' : '') + x;
+return (x < 10 ? '' : '') + x;
 }
-
-// Animation script
-function playAnimation() {
-  let tl = gsap.timeline();
-  tl
-    .to('.timer', {duration: 1, opacity: 0})
-    .to('#new-year-text path', { duration: 2, css: { fill: 'rgba(255,255,255,1)' }, ease: 'power4.outs', delay: '-2' })
- }
 
 const fireworks = new Fireworks(fireworkContainer, {
-  speed: 10,
-  acceleration: 1,
-  friction: 1,
-  gravity: 4,
-  particles: 400,
-  explosion: 10
+    speed: 20,
+    acceleration: 1,
+    friction: 1,
+    gravity: 4,
+    particles: 400,
+    explosion: 10
 })
